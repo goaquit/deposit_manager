95850c37bb553a8f26623cb6b0b597679c9d9bfe
andrey <ryzhov.a@alager.ru>
2019-09-11 17:09:00 +0300

    Test project for request to rest api server

--- tests/cpp_rest_sdk_framework_test.cpp
+++ tests/cpp_rest_sdk_framework_test.cpp
@@ -1,5 +1,14 @@
 #include <gtest/gtest.h>
 
+#include <fstream>
+#include <iostream>
+
+#include <cpprest/filestream.h>
+#include <cpprest/http_client.h>
+#include <cpprest/json.h>
+
+TEST(CppRestSdk, GettingStartHttpClient) { ASSERT_TRUE(true); }
+
 int main(int argc, char *argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
 
