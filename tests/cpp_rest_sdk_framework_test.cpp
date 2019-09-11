ec96c424fe9ddbb9d5a93d448b2cb0807547633d
andrey <ryzhov.a@alager.ru>
2019-09-11 18:20:13 +0300

    Test cpprestsdk framework

--- tests/cpp_rest_sdk_framework_test.cpp
+++ tests/cpp_rest_sdk_framework_test.cpp
@@ -1,15 +1,79 @@
 #include <gtest/gtest.h>
 
-#include <fstream>
-#include <iostream>
-
-#include <cpprest/filestream.h>
 #include <cpprest/http_client.h>
-#include <cpprest/json.h>
 
-TEST(CppRestSdk, GettingStartHttpClient) { ASSERT_TRUE(true); }
+TEST(Integration_CppRestSdk, GettingStartHttpClient) {
+  web::uri uri(U("https://reqres.in"));
 
-int main(int argc, char *argv[]) {
+  web::http::client::http_client client(uri);
+
+  web::uri_builder builder(U("/api/users"));
+
+  builder.append_query(U("page"), U("2"));
+
+  auto callback_process_response = [](web::http::http_response response) {
+    {
+      const auto actual_status_code = response.status_code();
+      int expected = 200;
+
+      ASSERT_EQ(actual_status_code, expected);
+    }
+
+    try {
+      auto json_obj = response.extract_json(true).get();
+
+      {
+        const auto actual_page = json_obj.at(U("page"));
+        int expected = 2;
+
+        ASSERT_EQ(actual_page, expected);
+      }
+
+      {
+        const auto actual_total_page = json_obj.at(U("total_pages"));
+        int expected = 2;
+
+        ASSERT_EQ(actual_total_page, expected);
+      }
+
+      auto data = json_obj.at(U("data")).as_array();
+
+      {
+        const auto actual_size = data.size();
+
+        int expected = 6;
+
+        ASSERT_EQ(actual_size, expected);
+      }
+
+      auto first_element = data.at(0).as_object();
+
+      bool is_finded = first_element.find(U("id")) != first_element.end();
+
+      ASSERT_TRUE(is_finded);
+
+      bool is_not_finded =
+          first_element.find(U("bad_key")) != first_element.end();
+
+      ASSERT_FALSE(is_not_finded);
+
+      std::cout << std::endl;
+    } catch (web::json::json_exception& e) {
+      FAIL() << "Json incorrect field" << e.what();
+    }
+  };
+
+  try {
+    client.request(web::http::methods::GET, builder.to_string())
+        .then(callback_process_response)
+        .wait();
+
+  } catch (std::exception& e) {
+    FAIL() << "Fail in http client request task " << e.what();
+  }
+}
+
+int main(int argc, char* argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
 
   return RUN_ALL_TESTS();
